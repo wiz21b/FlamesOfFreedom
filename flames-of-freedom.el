e2acff846dc7735c285f2f92a08233fdf663fc20
Stephane Champailler <schampailler@skynet.be>
2019-10-08 14:09:13 +0200

    Better/cleaner code, maybe faster

--- flames-of-freedom.el
+++ flames-of-freedom.el
@@ -87,16 +87,58 @@ Example : '(10 10 10 20 20 30) will give
   (insert (propertize text 'face '(:foreground "white" :background "black"))))
 
 
-(defun flames-of-freedom-default ()
-  "Displays the flames of freedom.
 
-These are the eternal flames of freedom (and an homage to RMS who
-is having tough times in this year 2019).
+(defun fof-update-flames (l flame-buffer-width flame-buffer-height time)
 
-A little poem is displayed."
-  (interactive)
-  (flames-of-freedom-my-message "These are the eternal flames of freedom,|Showing us light in darkness|beyond the thought police.|Software is our sword,|GPL the great ultimate.|"))
+  ;; The lowest line of the flames is random.  I don't update it
+  ;; too often so that the flames look nicer.
 
+  (if (= 0 (% time 5))
+      (let ((factor (if (< time flame-buffer-width)
+			(truncate (+ 1 (* 35 (/ (float time) flame-buffer-width))))
+		      36)))
+	(dotimes (i flame-buffer-width)
+	  (aset (aref l (- flame-buffer-height 1))
+		i (random factor)))))
+
+  ;; Just compute the flames as usual. Add a bit of randomness to
+  ;; make it look better.
+
+  (dotimes (i (- (length l) 1))
+    (let ( (v (aref l (+ i 1)))
+	   (v2 (aref l i))
+	   (width (- flame-buffer-width 2)))
+      (dotimes (z width)
+	(aset v2 (1+ z)
+	      (/
+	       (+ (aref v (+ 2 z)) (aref v (1+ z)) (aref v z)
+		  (aref v2 (1+ z)) (- (random 4) 1))
+	       4))))))
+
+
+(defun fof-flames-to-string (l)
+  "Build a big string representing the whole frame buffer.
+The string has properties that tell the colour of each
+of its characters."
+
+  ;; I trust emacs to propose best-in-class implementations of
+  ;; mapconcat
+
+  (mapconcat
+   (lambda (line)
+     (mapconcat
+      (lambda (x) (propertize
+	  	   (make-string (cdr x)
+	  			(aref fof-int-to-blocks (car x)))
+	  	   'face (aref fof-int-to-faces (car x)) ))
+      (fof-dups line)
+      ""))
+   l
+   "\n"))
+
+(byte-compile 'fof-dups)
+(byte-compile 'fof-update-flames)
+(byte-compile 'fof-flames-to-string)
 
 (defun flames-of-freedom-my-message (&optional the-message)
   "Displays the flames of freedom.
@@ -111,6 +153,8 @@ leave the message empty."
   (interactive "sMessage to show (sentences separated by |):")
 
   (let* ((window (selected-window))
+
+	 ;; -1 seems necessary for this to work in emacs-nw
 	 (flame-buffer-width (- (window-body-width window) 1))
 	 (flame-buffer-height (+ 3 (window-total-size)))
 	 (l (make-vector flame-buffer-height nil))
@@ -120,6 +164,9 @@ leave the message empty."
 	 (messages (vconcat (split-string the-message "|")))
 	 (current-msg 0)
 	 (last-time (float-time)))
+
+    ;; help testing by makeing excution results repeatable.
+    (random "alphabravo")
 
     (dotimes (i (length l))
       (aset l i (make-vector flame-buffer-width 0)))
@@ -135,32 +182,15 @@ leave the message empty."
 
       ;; Computing the flames
 
-      ;; The lowest line of the flames is random.  I don't update it
-      ;; too often so that the flames look nicer.
-
-      (if (= 0 (% time 5))
-	  (let ((factor (if (< time flame-buffer-width)
-			   (truncate (+ 1 (* 35 (/ (float time) flame-buffer-width))))
-			  36)))
-	    (dotimes (i flame-buffer-width)
-	      (aset (aref l (- flame-buffer-height 1))
-		    i (random factor)))))
-
-      ;; Just compute the flames as usual. Add a bit of randomness to
-      ;; make it look better.
-
-      (dotimes (i (- (length l) 1))
-	(let ( (v (aref l (+ i 1)))
-	       (v2 (aref l i))
-	       (width (- flame-buffer-width 2)))
-	  (dotimes (z width)
-	    (aset v2 (1+ z)
-		  (/
-		   (+ (aref v (+ 2 z)) (aref v (1+ z)) (aref v z)
-		      (aref v2 (1+ z)) (- (random 4) 1))
-		   4)))))
+      (fof-update-flames l flame-buffer-width flame-buffer-height time)
 
       ;; That's screen flipping in emacs :-)
+
+      ;; This seems to be on optimization. My interpretation is that
+      ;; when emacs hides a buffer, its display is not updated anymore
+      ;; (of course). So when I modify the buffer, I spare the
+      ;; updates. Proof : If I use just one buffer, things go much
+      ;; slower.
 
       (cond ((eq (current-buffer) buffer1)
 	     (set-window-buffer window buffer1 t)
@@ -169,35 +199,19 @@ leave the message empty."
 	     (set-window-buffer window buffer2 t)
 	     (set-buffer buffer1)))
 
-      ;; Remember emacs favor processing over display so if I don't ask,
-      ;; redisplay never get a chance to occur.
+      ;; Remember emacs favor processing/input over display so if I
+      ;; don't ask, redisplay never get a chance to occur.
 
       (redisplay)
 
       ;; Displaying the flames in the buffer
 
       (erase-buffer)
-      (goto-char 0)
 
-      (let ( (i 1) )
-	(while (< i (- (length l) 1))
+      ;; FIXME Test (setf (buffer-substring start end) new-string)
 
-	  (insert-before-markers
-	   (mapconcat
-	    (lambda (x) (propertize
-	  		 (make-string (cdr x)
-	  			      (aref fof-int-to-blocks (car x)))
-	  		 'face (aref fof-int-to-faces (car x)) ))
-	    (fof-dups (aref l i))
-	    ""))
-
-	  ;; FIXME The way I break a line introduces unwanted
-	  ;; spaces...
-
-	  (insert-char ?\n)
-	  (end-of-buffer)
-
-	  (setq i (+ i 1))))
+      (insert
+       (fof-flames-to-string (seq-take l (window-total-size))))
 
       ;; Display the messages in the middle of the screen
 
@@ -240,5 +254,18 @@ leave the message empty."
     ;; Make sure that the key the user has typed to exit our program
     ;; doesn't show up in its buffer
     (discard-input)))
+
+
+
+(defun flames-of-freedom-default ()
+  "Displays the flames of freedom.
+
+These are the eternal flames of freedom (and an homage to RMS who
+is having tough times in this year 2019).
+
+A little poem is displayed."
+  (interactive)
+  (flames-of-freedom-my-message "These are the eternal flames of freedom,|Showing us light in darkness|beyond the thought police.|Software is our sword,|GPL the great ultimate.|"))
+
 
 (flames-of-freedom-default)
