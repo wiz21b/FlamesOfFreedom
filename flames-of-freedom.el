d3c6a121dd5504c806caead09db1c7d245cd9d96
Stephane Champailler <schampailler@skynet.be>
2019-10-10 21:38:00 +0200

    Tried several optimization, added notes about that and kept the best ones

--- flames-of-freedom.el
+++ flames-of-freedom.el
@@ -1,3 +1,4 @@
+;; -*-lexical-binding: t;-*-
 ;;; flames-of-freedom.el --- The flames of freedom
 
 ;; Copyright (C) 2019 Stéphane Champailler
@@ -70,23 +71,27 @@ Example : '(10 10 10 20 20 30) will give
 
 (defconst fof-int-to-faces
   (fof-make-vector-by-step
-   '((1  . (:foreground "grey"   :background "black"))
-     (2  . (:foreground "grey"   :background "black"))
-     (2  . (:foreground "grey"   :background "black"))
-     (2  . (:foreground "orange" :background "grey"))
-     (3  . (:foreground "orange" :background "red"))
-     (4  . (:foreground "red"    :background "yellow"))
-     (4  . (:foreground "yellow" :background "yellow"))
-     (10 . (:foreground "white"  :background "yellow"))
-     (10 . (:foreground "white"  :background "white")))))
+   '((1  . (face (:foreground "grey"   :background "black")))
+     (2  . (face (:foreground "grey"   :background "black")))
+     (2  . (face (:foreground "grey"   :background "black")))
+     (2  . (face (:foreground "orange" :background "grey")))
+     (3  . (face (:foreground "orange" :background "red")))
+     (4  . (face (:foreground "red"    :background "yellow")))
+     (4  . (face (:foreground "yellow" :background "yellow")))
+     (10 . (face (:foreground "white"  :background "yellow")))
+     (10 . (face (:foreground "white"  :background "white"))))))
 
 (defun fof-write-line (text pos)
-  (goto-char 0)
+  (goto-char (point-min))
   (forward-line pos)
   (delete-region (point) (+ (point) (length text)))
   (insert (propertize text 'face '(:foreground "white" :background "black"))))
 
-
+(defun fof-make-flames-buffer (flame-buffer-width flame-buffer-height)
+  (let ( (l (make-vector flame-buffer-height nil)))
+    (dotimes (i flame-buffer-height)
+      (aset l i (make-vector flame-buffer-width 0)))
+    l))
 
 (defun fof-update-flames (l flame-buffer-width time)
 
@@ -116,7 +121,6 @@ Example : '(10 10 10 20 20 30) will give
 		  (aref v2 (1+ z)) (- (random 4) 1))
 	       4))))))
 
-
 (defun fof-flames-to-string (l)
   "Build a big string representing the whole frame buffer.
 The string has properties that tell the colour of each
@@ -137,13 +141,75 @@ of its characters."
    l
    "\n"))
 
-;; This accelerates things by +/- 7%
+(defun fof-flames-to-string (l)
+
+  (let* ((width-base (length (aref l 1)))
+	 (width (+ 1 (length (aref l 1))))
+	 (bigv (make-vector (* (length l) width) 0)) ; Recreating the vector is not signifcantly slower.
+
+	 (i 0))
+    (dotimes (y (length l))
+
+      (let ((line (aref l y)))
+	(dotimes (x width-base)
+	  (aset bigv (+ i x) (aref fof-int-to-blocks (aref line x)))))
+
+      (setq i (+ width i -1))
+      (aset bigv i ?\n )
+      (setq i (1+ i)))
+
+  ;; Doing one big concat is faster than concatenating several strings together
+  ;; Remember concat takes characters and makes strings out of them.
+  (concat bigv)))
+
+
+;; Things I tried to improve speed :
+
+;; * A bit of profiling indicates that removing the call to
+;;   put-text-property increases the speed by 50%.
+
+;; * Using put-text-property directly on buffer seems faster than
+;;   putting properties on strings and then inserting the string+props
+;;   in the buffer
+
+;; * set-text-properties is not faster then put-text-property
+;;   in my case (but set-text-properties is cleaner).
+
+;; * I tried to defsubst the lambda expression => no noticeable speed
+;;   improvement.
+
+;; * I tried to transform i into a global variable (defvar) => no
+;;   noticeable speed improvement.
+
+;; * I tried double buffering (hypothesis, emacs displays a buffer
+;;   more efficitenly when it's a window refresh. Bad hypothesis, this
+;;   makes things significantly slower.
+
+;; * Byte compiling does make a noticeable improvement.
+
+;; * Moved face construction outside the loop. Cleaner code, performance
+;;   are the same.
+
+(defun fof-flames-to-string-props (l)
+  (let ((i 1))
+    (dotimes (y (length l))
+
+      (let ((line-props (fof-dups (aref l y))))
+	(mapc (lambda (p)
+		(set-text-properties i (+ i (cdr p)) (aref fof-int-to-faces (car p)))
+		(setq i (+ i (cdr p))))
+	      line-props))
+      ;; skip \n at the end of each line
+      (setq i (+ i 1)))))
+
+;; This accelerates things by +/- 20%
 (byte-compile 'fof-dups)
 (byte-compile 'fof-update-flames)
 (byte-compile 'fof-flames-to-string)
+(byte-compile 'fof-flames-to-string-props)
 
 (defun flames-of-freedom-my-message (&optional the-message testing)
-  "Displays the flames of freedom.
+  "Displays the Flames of Freedom.
 
 These are the eternal flames of freedom (and an homage to RMS who
 is having tough times in this year 2019).
@@ -157,27 +223,29 @@ leave the message empty."
   (let* ((window (selected-window))
 
 	 ;; -1 seems necessary for this to work in emacs-nw
-	 (flame-buffer-width (- (window-body-width window) 1))
+	 (flame-buffer-width (- (window-body-width window) 0))
 	 (flame-buffer-height (+ 3 (window-total-size)))
-	 (l (make-vector flame-buffer-height nil))
+	 (l (fof-make-flames-buffer flame-buffer-width flame-buffer-height))
 	 (buffer1 (get-buffer-create "Flames Of Freedom"))
 	 (messages (vconcat (split-string the-message "|")))
 	 (current-msg 0)
 	 (drawn-frames 0)
 	 (drawn-frames-benchmarking 0)
 	 (start-time (float-time))
-	 (passed-time 0)
 	 (last-time (float-time)))
+
+    (if testing
+	(message (format "%d x %d = %d" flame-buffer-width flame-buffer-height (* flame-buffer-width flame-buffer-height))))
 
     ;; help testing by makeing excution results repeatable.
     (if testing (random "alphabravo"))
 
-    (dotimes (i (length l))
-      (aset l i (make-vector flame-buffer-width 0)))
-
     (buffer-disable-undo buffer1)
     (set-window-buffer window buffer1)
     (set-buffer buffer1)
+
+    ;; Make the cursor disappears
+    (setq cursor-type nil)
 
     (while (not (input-pending-p))
 
@@ -185,19 +253,17 @@ leave the message empty."
 
       (fof-update-flames l flame-buffer-width drawn-frames)
 
-      ;; Displaying the flames in the buffer (actually replacing the
-      ;; content of the buffer). This avoids double buffering but
-      ;; doesn't make things significally faster.
+      ;; Displaying the flames in the buffer.
 
-      (let ((big-string (fof-flames-to-string
-			 (seq-take l (min (length l) (window-total-size))))))
-	(if (> (buffer-size) 1000)
-	    (progn
-	      ;; buffer-substring "end" index is not inclusive (see emacs
-	      ;; documentation example)
-	      (setf (buffer-substring 1 (+ (length big-string) 1)) big-string))
-	  (progn
-	    (insert big-string))))
+      ;; * Replacing the buffer (setf (buffer-substring ...))
+      ;;   instead of erasing/recreating it is not faster.
+      ;; * Double buffering is slower.
+
+      (let* ((sub-vec (seq-take l (min (length l) (window-total-size))))
+	     (big-string (fof-flames-to-string sub-vec)))
+	(erase-buffer)
+	(insert big-string)
+	(fof-flames-to-string-props sub-vec))
 
       ;; Display the messages in the middle of the screen
 
@@ -228,24 +294,21 @@ leave the message empty."
 
       ;; Make sure the window will display the buffer from its top
 
-      (set-window-start window 0) ;; !!! This results in a 10% performance improvement. Dont't know why
+      (set-window-start window 1) ;; !!! This results in a 10% performance improvement. Dont't know why
 
       ;; Remember emacs favor processing/input over display so if I
       ;; don't ask, redisplay never get a chance to occur.
 
-      (goto-char 1) ;; Dunno why, but other values don't work as expected
       (redisplay)
 
-      (if (and testing (< passed-time 10))
-	  (progn
-	    (setq passed-time (- (float-time) start-time))
-	    (setq drawn-frames-benchmarking (1+ drawn-frames-benchmarking))))
+      (if (and testing (= drawn-frames-benchmarking 0))
+	  (let ((passed-time (- (float-time) start-time)))
+	    (if (> passed-time 10)
+		(progn
+		  (setq drawn-frames-benchmarking drawn-frames)
+		  (message (format "%d frames drawn in %d seconds,  %f fps" drawn-frames-benchmarking passed-time (/ drawn-frames passed-time)))))))
 
       (setq drawn-frames (+ 1 drawn-frames)))
-
-    (if testing
-	(message (format "%d frames drawn in %d seconds,  %f fps" drawn-frames-benchmarking passed-time (/ drawn-frames passed-time))))
-
 
     (kill-buffer buffer1)
 
@@ -256,7 +319,7 @@ leave the message empty."
 
 
 (defun flames-of-freedom-default ()
-  "Displays the flames of freedom.
+  "Displays the Flames of Freedom.
 
 These are the eternal flames of freedom (and an homage to RMS who
 is having tough times in this year 2019).
