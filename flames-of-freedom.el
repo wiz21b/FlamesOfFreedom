67b5242553e1fe7225612de0ab17d6392ab25e78
Stéphane Champailler <schampailler@skynet.bewq>
2019-12-01 14:02:46 +0100

    branch to publish to melpa; changed version to reflect melpa maintainer's comments

--- flames-of-freedom.el
+++ flames-of-freedom.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2019 Stéphane Champailler
 
 ;; Author: Stéphane Champailler <schampailler@skynet.be>
-;; Package-Version: 1.0
+;; Package-Version: 1.1
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: multimedia
 ;; URL: https://github.com/wiz21b/FlamesOfFreedom
